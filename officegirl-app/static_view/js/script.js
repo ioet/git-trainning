93070345b93c3f8466e8365b80aaf69b09a2e26b
Jhymer Martinez <jamartinezg@unl.edu.ec>
2015-04-23 22:39:59 -0500

    Mejorado el footer para evitar sobreponerlo al formulario

--- officegirl-app/static_view/js/script.js
+++ officegirl-app/static_view/js/script.js
@@ -1,10 +1,33 @@
+
+/**
+ * Adapta el footer de acuerdo 
+ * al tamaño de la ventana
+ */
+function onRezize(){
+	onLoad();
+}
+
+function onLoad(){
+	check.checked = false;
+	var w = $(window).height();
+	if($(window).height() <= 680){
+		var s = -140 +(680-w);
+		$('.wrapper ').css('margin-bottom',s+'px');
+	}else{
+		$('.wrapper ').css('margin-bottom','-140px');
+	}
+}
+
+
 function showContent() {
-        element = document.getElementById("content");
-        check = document.getElementById("check");
-        if (check.checked) {
-            element.style.display='block';
-        }
-        else {
-            element.style.display='none';
-        }
+    element = document.getElementById("content");
+    check = document.getElementById("check");
+    if (check.checked) {
+        element.style.display='block';
     }
+    else {
+        element.style.display='none';
+    }
+}
+
+
